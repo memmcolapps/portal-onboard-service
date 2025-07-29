2959ada3ac2d3ce0edd400e4c9002dba1e4527a6
H.Soliu <abeeb.ilias@gmail.com>
2025-07-29 01:18:44 -0700

    Initial commit

---
+++ src/main/java/org/memmcol/portalonboardservice/PortalOnboardServiceApplication.java
@@ -0,0 +1,13 @@
+package org.memmcol.portalonboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalOnboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalOnboardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/memmcol/portalonboardservice/PortalOnboardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.memmcol.portalonboardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalOnboardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
