a66126c96ff18f0996c66af97a961fdef26102e3
Musa001-web <ajibadetemilade@gmail.com>
2025-09-10 11:54:49 +0100

    Feature: Audit Log

--- src/main/java/org/memmcol/portalonboardservice/repository/AuditRepository.java
+++ src/main/java/org/memmcol/portalonboardservice/repository/AuditRepository.java
@@ -1,6 +1,8 @@
 package org.memmcol.portalonboardservice.repository;
 
 import org.memmcol.portalonboardservice.model.audit.AuditLog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.UUID;
 
 public interface AuditRepository extends MongoRepository<AuditLog, String> {
     List<AuditLog> findTop5ByCreator_IdOrderByCreatedAtDesc(UUID orgId);
+    List<AuditLog> findAllByCreator_Id(UUID orgId);
+    Page<AuditLog> findAllByCreator_IdOrderByCreatedAtDesc(UUID orgId, Pageable pageable);
 }
---
+++ src/main/java/org/memmcol/portalonboardservice/service/auditlog/AuditLogServiceImpl.java
@@ -0,0 +1,110 @@
+package org.memmcol.portalonboardservice.service.auditlog;
+
+import org.memmcol.portalonboardservice.config.ResponseProperties;
+import org.memmcol.portalonboardservice.model.audit.AuditLog;
+import org.memmcol.portalonboardservice.model.audit.AuditLogDto;
+import org.memmcol.portalonboardservice.model.audit.ExceptionErrorLogs;
+import org.memmcol.portalonboardservice.model.user.Operator;
+import org.memmcol.portalonboardservice.model.user.Role;
+import org.memmcol.portalonboardservice.model.user.UserModel;
+import org.memmcol.portalonboardservice.repository.AuditRepository;
+import org.memmcol.portalonboardservice.repository.ExceptionAuditRepository;
+import org.memmcol.portalonboardservice.util.ResponseMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.memmcol.portalonboardservice.components.handleValidUser.handleUserValidation;
+
+@Service
+public class AuditLogServiceImpl implements AuditLogService {
+
+    private static final Logger log = LoggerFactory.getLogger(AuditLogServiceImpl.class);
+
+    private final AuditRepository auditRepository;
+
+    @Autowired
+    private ResponseProperties status;
+
+    @Autowired
+    private ExceptionAuditRepository exceptionAuditRepository;
+
+    public AuditLogServiceImpl(AuditRepository auditRepository) {
+        this.auditRepository = auditRepository;
+    }
+
+    @Override
+    public Map<String, Object> getAuditLog(int page, int size) {
+        try {
+            Operator um = handleUserValidation();
+            Map<String, Object> response = new HashMap<>();
+
+            // If page or size is null, 0, or less than 1, fetch all
+            if (page < 0 || size <= 0) {
+                List<AuditLogDto> result = auditRepository.findAllByCreator_Id(um.getId())
+                        .stream()
+                        .map(log -> new AuditLogDto(
+                                log.getId(),
+                                log.getType(),
+                                log.getCreator().getFirstname() + " " + log.getCreator().getLastname(),
+                                log.getCreator().getEmail(),
+                                log.getCreator().getRoles().get(0).getUserRole(),
+                                log.getDescription(),
+                                log.getUserAgent(),
+                                log.getIpAddress(),
+                                log.getCreatedAt(),
+                                log.getReason()
+                        ))
+                        .collect(Collectors.toList());
+                response.put("data", result);
+                response.put("size", result.size());
+
+            } else {
+                Pageable pageable = PageRequest.of(page, size, Sort.by("createdAt").descending());
+                Page<AuditLog> pagedResult = auditRepository.findAllByCreator_IdOrderByCreatedAtDesc(um.getId(),pageable);
+
+                List<AuditLogDto> result = pagedResult.getContent().stream()
+                        .map(log -> new AuditLogDto(
+                                log.getId(),
+                                log.getType(),
+                                log.getCreator().getFirstname() + " " + log.getCreator().getLastname(),
+                                log.getCreator().getEmail(),
+                                log.getCreator().getRoles().get(0).getUserRole(),
+                                log.getDescription(),
+                                log.getUserAgent(),
+                                log.getIpAddress(),
+                                log.getCreatedAt(),
+                                log.getReason()
+                        ))
+                        .collect(Collectors.toList());
+                response.put("data", result);
+                response.put("page", pagedResult.getNumber());
+                response.put("totalData", pagedResult.getTotalElements());
+                response.put("size", pagedResult.getTotalPages());
+            }
+            return ResponseMap.response(status.getSuccessCode(), "Logs " + status.getDesc(), response);
+
+        } catch (Exception exception) {
+            log.error("Error occurred while fetching audit logs: {}", exception.getMessage().trim(), exception);
+
+            ExceptionErrorLogs exceptionErrorLogs = new ExceptionErrorLogs();
+            exceptionErrorLogs.setDescription("Error occurred while fetching audit logs");
+            exceptionErrorLogs.setError_message(exception.getMessage().trim());
+            exceptionErrorLogs.setError(exception.toString().trim());
+            exceptionAuditRepository.save(exceptionErrorLogs);
+
+            throw exception;
+        }
+    }
+
+}
