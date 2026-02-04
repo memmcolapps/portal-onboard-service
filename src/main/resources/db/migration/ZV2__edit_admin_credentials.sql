755952b9e73248f863439e5095961b9fcc0b1d2e
H.Soliu <abeeb.ilias@gmail.com>
2026-02-04 16:12:47 +0100

    run data migration -  create user

--- src/main/resources/db/migration/ZV2__edit_admin_credentials.sql
+++
@@ -1,7 +0,0 @@
--- Update password for existing operator
-UPDATE portal_users
-SET password = '$2a$10$vpdTuh9gul9V3YTye2DNXeKOz4QKU1JFssIgcXm0YSrn.ySonEnrO',
-    updated_at = NOW()
-WHERE email = 'memmcolapp@gmail.com';
-
---$2a$10$vpdTuh9gul9V3YTye2DNXeKOz4QKU1JFssIgcXm0YSrn.ySonEnrO
